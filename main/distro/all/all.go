87bc5fa46f605298575a505d7c051ac97a2b5ab0
kyze8439690 <yugy1991@gmail.com>
2023-10-06 21:15:38 +0800

    remove blackhole

--- main/distro/all/all.go
+++ main/distro/all/all.go
@@ -34,7 +34,6 @@ import (
 	_ "github.com/xtls/xray-core/app/observatory"
 
 	// Inbound and outbound proxies.
-	_ "github.com/xtls/xray-core/proxy/blackhole"
 	_ "github.com/xtls/xray-core/proxy/dns"
 	_ "github.com/xtls/xray-core/proxy/dokodemo"
 	_ "github.com/xtls/xray-core/proxy/freedom"
