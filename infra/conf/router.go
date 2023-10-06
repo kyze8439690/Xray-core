49a5c67dbe54d8a8b4caf8d2b0a5c44c3b3f7f32
kyze8439690 <yugy1991@gmail.com>
2023-10-06 21:15:39 +0800

    remove rule matcher

--- infra/conf/router.go
+++ infra/conf/router.go
@@ -2,13 +2,9 @@ package conf
 
 import (
 	"encoding/json"
-	"strconv"
 	"strings"
 
 	"github.com/xtls/xray-core/app/router"
-	"github.com/xtls/xray-core/common/net"
-	"github.com/xtls/xray-core/common/serial"
-	"github.com/xtls/xray-core/common/platform/filesystem"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -23,88 +19,13 @@ type StrategyConfig struct {
 	Settings *json.RawMessage `json:"settings"`
 }
 
-type BalancingRule struct {
-	Tag         string         `json:"tag"`
-	Selectors   StringList     `json:"selector"`
-	Strategy    StrategyConfig `json:"strategy"`
-	FallbackTag string         `json:"fallbackTag"`
-}
-
-// Build builds the balancing rule
-func (r *BalancingRule) Build() (*router.BalancingRule, error) {
-	if r.Tag == "" {
-		return nil, newError("empty balancer tag")
-	}
-	if len(r.Selectors) == 0 {
-		return nil, newError("empty selector list")
-	}
-
-	r.Strategy.Type = strings.ToLower(r.Strategy.Type)
-	switch r.Strategy.Type {
-	case "":
-		r.Strategy.Type = strategyRandom
-	case strategyRandom, strategyLeastLoad, strategyLeastPing, strategyRoundRobin:
-	default:
-		return nil, newError("unknown balancing strategy: " + r.Strategy.Type)
-	}
-
-	settings := []byte("{}")
-	if r.Strategy.Settings != nil {
-		settings = ([]byte)(*r.Strategy.Settings)
-	}
-	rawConfig, err := strategyConfigLoader.LoadWithID(settings, r.Strategy.Type)
-	if err != nil {
-		return nil, newError("failed to parse to strategy config.").Base(err)
-	}
-	var ts proto.Message
-	if builder, ok := rawConfig.(Buildable); ok {
-		ts, err = builder.Build()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return &router.BalancingRule{
-		Strategy:         r.Strategy.Type,
-		StrategySettings: serial.ToTypedMessage(ts),
-		FallbackTag:      r.FallbackTag,
-		OutboundSelector: r.Selectors,
-		Tag:              r.Tag,
-	}, nil
-}
-
 type RouterConfig struct {
 	Settings       *RouterRulesConfig `json:"settings"` // Deprecated
 	RuleList       []json.RawMessage  `json:"rules"`
-	DomainStrategy *string            `json:"domainStrategy"`
-	Balancers      []*BalancingRule   `json:"balancers"`
-
-	DomainMatcher string `json:"domainMatcher"`
-}
-
-func (c *RouterConfig) getDomainStrategy() router.Config_DomainStrategy {
-	ds := ""
-	if c.DomainStrategy != nil {
-		ds = *c.DomainStrategy
-	} else if c.Settings != nil {
-		ds = c.Settings.DomainStrategy
-	}
-
-	switch strings.ToLower(ds) {
-	case "alwaysip":
-		return router.Config_UseIp
-	case "ipifnonmatch":
-		return router.Config_IpIfNonMatch
-	case "ipondemand":
-		return router.Config_IpOnDemand
-	default:
-		return router.Config_AsIs
-	}
 }
 
 func (c *RouterConfig) Build() (*router.Config, error) {
 	config := new(router.Config)
-	config.DomainStrategy = c.getDomainStrategy()
 
 	var rawRuleList []json.RawMessage
 	if c != nil {
@@ -121,18 +42,7 @@ func (c *RouterConfig) Build() (*router.Config, error) {
 			return nil, err
 		}
 
-		if rule.DomainMatcher == "" {
-			rule.DomainMatcher = c.DomainMatcher
-		}
-
 		config.Rule = append(config.Rule, rule)
-	}
-	for _, rawBalancer := range c.Balancers {
-		balancer, err := rawBalancer.Build()
-		if err != nil {
-			return nil, err
-		}
-		config.BalancingRule = append(config.BalancingRule, balancer)
 	}
 	return config, nil
 }
@@ -140,206 +50,19 @@ func (c *RouterConfig) Build() (*router.Config, error) {
 type RouterRule struct {
 	Type        string `json:"type"`
 	OutboundTag string `json:"outboundTag"`
-	BalancerTag string `json:"balancerTag"`
-
-	DomainMatcher string `json:"domainMatcher"`
-}
-
-func ParseIP(s string) (*router.CIDR, error) {
-	var addr, mask string
-	i := strings.Index(s, "/")
-	if i < 0 {
-		addr = s
-	} else {
-		addr = s[:i]
-		mask = s[i+1:]
-	}
-	ip := net.ParseAddress(addr)
-	switch ip.Family() {
-	case net.AddressFamilyIPv4:
-		bits := uint32(32)
-		if len(mask) > 0 {
-			bits64, err := strconv.ParseUint(mask, 10, 32)
-			if err != nil {
-				return nil, newError("invalid network mask for router: ", mask).Base(err)
-			}
-			bits = uint32(bits64)
-		}
-		if bits > 32 {
-			return nil, newError("invalid network mask for router: ", bits)
-		}
-		return &router.CIDR{
-			Ip:     []byte(ip.IP()),
-			Prefix: bits,
-		}, nil
-	case net.AddressFamilyIPv6:
-		bits := uint32(128)
-		if len(mask) > 0 {
-			bits64, err := strconv.ParseUint(mask, 10, 32)
-			if err != nil {
-				return nil, newError("invalid network mask for router: ", mask).Base(err)
-			}
-			bits = uint32(bits64)
-		}
-		if bits > 128 {
-			return nil, newError("invalid network mask for router: ", bits)
-		}
-		return &router.CIDR{
-			Ip:     []byte(ip.IP()),
-			Prefix: bits,
-		}, nil
-	default:
-		return nil, newError("unsupported address for router: ", s)
-	}
 }
 
 var (
 	FileCache = make(map[string][]byte)
 )
 
-func loadFile(file string) ([]byte, error) {
-	if FileCache[file] == nil {
-		bs, err := filesystem.ReadAsset(file)
-		if err != nil {
-			return nil, newError("failed to open file: ", file).Base(err)
-		}
-		if len(bs) == 0 {
-			return nil, newError("empty file: ", file)
-		}
-		// Do not cache file, may save RAM when there
-		// are many files, but consume CPU each time.
-		return bs, nil
-		FileCache[file] = bs
-	}
-	return FileCache[file], nil
-}
-
-func find(data, code []byte) []byte {
-	codeL := len(code)
-	if codeL == 0 {
-		return nil
-	}
-	for {
-		dataL := len(data)
-		if dataL < 2 {
-			return nil
-		}
-		x, y := DecodeVarint(data[1:])
-		if x == 0 && y == 0 {
-			return nil
-		}
-		headL, bodyL := 1+y, int(x)
-		dataL -= headL
-		if dataL < bodyL {
-			return nil
-		}
-		data = data[headL:]
-		if int(data[1]) == codeL {
-			for i := 0; i < codeL && data[2+i] == code[i]; i++ {
-				if i+1 == codeL {
-					return data[:bodyL]
-				}
-			}
-		}
-		if dataL == bodyL {
-			return nil
-		}
-		data = data[bodyL:]
-	}
-}
-
-type AttributeMatcher interface {
-	Match(*router.Domain) bool
-}
-
-type BooleanMatcher string
-
-func (m BooleanMatcher) Match(domain *router.Domain) bool {
-	for _, attr := range domain.Attribute {
-		if attr.Key == string(m) {
-			return true
-		}
-	}
-	return false
-}
-
-type AttributeList struct {
-	matcher []AttributeMatcher
-}
-
-func (al *AttributeList) Match(domain *router.Domain) bool {
-	for _, matcher := range al.matcher {
-		if !matcher.Match(domain) {
-			return false
-		}
-	}
-	return true
-}
-
-func (al *AttributeList) IsEmpty() bool {
-	return len(al.matcher) == 0
-}
-
-func parseAttrs(attrs []string) *AttributeList {
-	al := new(AttributeList)
-	for _, attr := range attrs {
-		lc := strings.ToLower(attr)
-		al.matcher = append(al.matcher, BooleanMatcher(lc))
-	}
-	return al
-}
-
-func parseDomainRule(domain string) ([]*router.Domain, error) {
-	domainRule := new(router.Domain)
-	switch {
-	case strings.HasPrefix(domain, "regexp:"):
-		domainRule.Type = router.Domain_Regex
-		domainRule.Value = domain[7:]
-
-	case strings.HasPrefix(domain, "domain:"):
-		domainRule.Type = router.Domain_Domain
-		domainRule.Value = domain[7:]
-
-	case strings.HasPrefix(domain, "full:"):
-		domainRule.Type = router.Domain_Full
-		domainRule.Value = domain[5:]
-
-	case strings.HasPrefix(domain, "keyword:"):
-		domainRule.Type = router.Domain_Plain
-		domainRule.Value = domain[8:]
-
-	case strings.HasPrefix(domain, "dotless:"):
-		domainRule.Type = router.Domain_Regex
-		switch substr := domain[8:]; {
-		case substr == "":
-			domainRule.Value = "^[^.]*$"
-		case !strings.Contains(substr, "."):
-			domainRule.Value = "^[^.]*" + substr + "[^.]*$"
-		default:
-			return nil, newError("substr in dotless rule should not contain a dot: ", substr)
-		}
-
-	default:
-		domainRule.Type = router.Domain_Plain
-		domainRule.Value = domain
-	}
-	return []*router.Domain{domainRule}, nil
-}
-
 func parseFieldRule(msg json.RawMessage) (*router.RoutingRule, error) {
 	type RawFieldRule struct {
 		RouterRule
-		Domain     *StringList       `json:"domain"`
-		Domains    *StringList       `json:"domains"`
 		IP         *StringList       `json:"ip"`
-		Port       *PortList         `json:"port"`
-		Network    *NetworkList      `json:"network"`
 		SourceIP   *StringList       `json:"source"`
-		SourcePort *PortList         `json:"sourcePort"`
-		User       *StringList       `json:"user"`
 		InboundTag *StringList       `json:"inboundTag"`
 		Protocols  *StringList       `json:"protocol"`
-		Attributes map[string]string `json:"attrs"`
 	}
 	rawFieldRule := new(RawFieldRule)
 	err := json.Unmarshal(msg, rawFieldRule)
@@ -353,54 +76,8 @@ func parseFieldRule(msg json.RawMessage) (*router.RoutingRule, error) {
 		rule.TargetTag = &router.RoutingRule_Tag{
 			Tag: rawFieldRule.OutboundTag,
 		}
-	case len(rawFieldRule.BalancerTag) > 0:
-		rule.TargetTag = &router.RoutingRule_BalancingTag{
-			BalancingTag: rawFieldRule.BalancerTag,
-		}
 	default:
 		return nil, newError("neither outboundTag nor balancerTag is specified in routing rule")
-	}
-
-	if rawFieldRule.DomainMatcher != "" {
-		rule.DomainMatcher = rawFieldRule.DomainMatcher
-	}
-
-	if rawFieldRule.Domain != nil {
-		for _, domain := range *rawFieldRule.Domain {
-			rules, err := parseDomainRule(domain)
-			if err != nil {
-				return nil, newError("failed to parse domain rule: ", domain).Base(err)
-			}
-			rule.Domain = append(rule.Domain, rules...)
-		}
-	}
-
-	if rawFieldRule.Domains != nil {
-		for _, domain := range *rawFieldRule.Domains {
-			rules, err := parseDomainRule(domain)
-			if err != nil {
-				return nil, newError("failed to parse domain rule: ", domain).Base(err)
-			}
-			rule.Domain = append(rule.Domain, rules...)
-		}
-	}
-
-	if rawFieldRule.Port != nil {
-		rule.PortList = rawFieldRule.Port.Build()
-	}
-
-	if rawFieldRule.Network != nil {
-		rule.Networks = rawFieldRule.Network.Build()
-	}
-
-	if rawFieldRule.SourcePort != nil {
-		rule.SourcePortList = rawFieldRule.SourcePort.Build()
-	}
-
-	if rawFieldRule.User != nil {
-		for _, s := range *rawFieldRule.User {
-			rule.UserEmail = append(rule.UserEmail, s)
-		}
 	}
 
 	if rawFieldRule.InboundTag != nil {
@@ -413,10 +90,6 @@ func parseFieldRule(msg json.RawMessage) (*router.RoutingRule, error) {
 		for _, s := range *rawFieldRule.Protocols {
 			rule.Protocol = append(rule.Protocol, s)
 		}
-	}
-
-	if len(rawFieldRule.Attributes) > 0 {
-		rule.Attributes = rawFieldRule.Attributes
 	}
 
 	return rule, nil
