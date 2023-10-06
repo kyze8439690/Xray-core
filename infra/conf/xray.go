89e33f922af4f42d1028a8773d56791ed867e79d
kyze8439690 <yugy1991@gmail.com>
2023-10-06 21:15:38 +0800

    remove dns proxy

--- infra/conf/xray.go
+++ infra/conf/xray.go
@@ -28,8 +28,6 @@ var (
 		"socks":       func() interface{} { return new(SocksClientConfig) },
 		"vmess":       func() interface{} { return new(VMessOutboundConfig) },
 		"trojan":      func() interface{} { return new(TrojanClientConfig) },
-		"dns":         func() interface{} { return new(DNSOutboundConfig) },
-		"wireguard":   func() interface{} { return new(WireGuardConfig) },
 	}, "protocol", "settings")
 
 	ctllog = log.New(os.Stderr, "xctl> ", 0)
