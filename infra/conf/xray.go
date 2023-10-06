7575d9d4f57a3e2b48b68895a285a11e2a177100
kyze8439690 <yugy1991@gmail.com>
2023-10-06 21:15:38 +0800

    remove freedom

--- infra/conf/xray.go
+++ infra/conf/xray.go
@@ -24,7 +24,6 @@ var (
 	}, "protocol", "settings")
 
 	outboundConfigLoader = NewJSONConfigLoader(ConfigCreatorCache{
-		"freedom":     func() interface{} { return new(FreedomConfig) },
 		"http":        func() interface{} { return new(HTTPClientConfig) },
 		"socks":       func() interface{} { return new(SocksClientConfig) },
 		"vmess":       func() interface{} { return new(VMessOutboundConfig) },
