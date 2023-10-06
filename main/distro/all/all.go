114c8e33fa96386bc95d1f11cade4b500ff6695b
kyze8439690 <yugy1991@gmail.com>
2023-10-06 21:15:39 +0800

    remove observatory

--- main/distro/all/all.go
+++ main/distro/all/all.go
@@ -18,9 +18,6 @@ import (
 	// Fix dependency cycle caused by core import in internet package
 	_ "github.com/xtls/xray-core/transport/internet/tagged/taggedimpl"
 
-	// Developer preview features
-	_ "github.com/xtls/xray-core/app/observatory"
-
 	// Inbound and outbound proxies.
 	_ "github.com/xtls/xray-core/proxy/http"
 	_ "github.com/xtls/xray-core/proxy/socks"
