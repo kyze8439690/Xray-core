a6d51de441954a43921de642835ff45114a75671
kyze8439690 <yugy1991@gmail.com>
2023-10-06 21:15:38 +0800

    remove loopback

--- main/distro/all/all.go
+++ main/distro/all/all.go
@@ -37,7 +37,6 @@ import (
 	_ "github.com/xtls/xray-core/proxy/dns"
 	_ "github.com/xtls/xray-core/proxy/freedom"
 	_ "github.com/xtls/xray-core/proxy/http"
-	_ "github.com/xtls/xray-core/proxy/loopback"
 	_ "github.com/xtls/xray-core/proxy/socks"
 	_ "github.com/xtls/xray-core/proxy/trojan"
 	_ "github.com/xtls/xray-core/proxy/vmess/inbound"
