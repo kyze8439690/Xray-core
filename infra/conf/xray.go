c60fdb1e6d71fcb35419960aecc0763152af0f2a
kyze8439690 <yugy1991@gmail.com>
2023-10-06 21:15:38 +0800

    remove trojan inbound

--- infra/conf/xray.go
+++ infra/conf/xray.go
@@ -19,8 +19,6 @@ import (
 var (
 	inboundConfigLoader = NewJSONConfigLoader(ConfigCreatorCache{
 		"socks":         func() interface{} { return new(SocksServerConfig) },
-		"trojan":        func() interface{} { return new(TrojanServerConfig) },
-		"wireguard":     func() interface{} { return &WireGuardConfig{IsClient: false} },
 	}, "protocol", "settings")
 
 	outboundConfigLoader = NewJSONConfigLoader(ConfigCreatorCache{
