b0b926f856e82d60cc52b14562f102d4b9e97202
kyze8439690 <yugy1991@gmail.com>
2023-10-06 21:15:39 +0800

    fix utls

--- transport/internet/tls/tls.go
+++ transport/internet/tls/tls.go
@@ -178,38 +178,8 @@ func GetFingerprint(name string) (fingerprint *utls.ClientHelloID) {
 	return
 }
 
-var PresetFingerprints = map[string]*utls.ClientHelloID{
-	// Recommended preset options in GUI clients
-	"chrome":     &utls.HelloChrome_Auto,
-	"firefox":    &utls.HelloFirefox_Auto,
-	"safari":     &utls.HelloSafari_Auto,
-	"ios":        &utls.HelloIOS_Auto,
-	"android":    &utls.HelloAndroid_11_OkHttp,
-	"edge":       &utls.HelloEdge_Auto,
-	"360":        &utls.Hello360_Auto,
-	"qq":         &utls.HelloQQ_Auto,
-	"random":     nil,
-	"randomized": nil,
-}
-
-var ModernFingerprints = map[string]*utls.ClientHelloID{
-	// One of these will be chosen as `random` at startup
-	"hellofirefox_99":         &utls.HelloFirefox_99,
-	"hellofirefox_102":        &utls.HelloFirefox_102,
-	"hellofirefox_105":        &utls.HelloFirefox_105,
-	"hellochrome_83":          &utls.HelloChrome_83,
-	"hellochrome_87":          &utls.HelloChrome_87,
-	"hellochrome_96":          &utls.HelloChrome_96,
-	"hellochrome_100":         &utls.HelloChrome_100,
-	"hellochrome_102":         &utls.HelloChrome_102,
-	"hellochrome_106_shuffle": &utls.HelloChrome_106_Shuffle,
-	"helloios_13":             &utls.HelloIOS_13,
-	"helloios_14":             &utls.HelloIOS_14,
-	"helloedge_85":            &utls.HelloEdge_85,
-	"helloedge_106":           &utls.HelloEdge_106,
-	"hellosafari_16_0":        &utls.HelloSafari_16_0,
-	"hello360_11_0":           &utls.Hello360_11_0,
-	"helloqq_11_1":            &utls.HelloQQ_11_1,
+var Fingerprints = map[string]*utls.ClientHelloID{
+	"randomized_no_alpn": &utls.HelloRandomizedNoALPN,
 }
 
 var OtherFingerprints = map[string]*utls.ClientHelloID{
